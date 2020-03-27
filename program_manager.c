c5501efbe99da52345ecc4e501323d6a711f4235
Ghlakshman <55837009+Ghlakshman@users.noreply.github.com>
2020-03-27 17:29:37 +0530

    Update program_manager.c

--- program_manager.c
+++ program_manager.c
@@ -2,58 +2,49 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<pthread.h>
-
-int pid_array[900];//min pid =100 and max pid = 1000 datastructure size is 900.
+int pid_array[900];
 
 int allocate_map()
 {
-  int result;//return value
-  
-  for(int i=0;i<900;i++)//allocating zero to empty sspaces so that it can be used to manage pid's
-  {
-      pid_array[i]=0;//allocating value zero
-  }
-  
-  for(int i=0;i<900;i++)//checking the allocation
-  {
-      if(pid_array[i]!=0)result = 0;
-      else result = 1;
-  }
-  return result;
-}
-
-
-int allocate_pid()
+int result;
+for(int i=0;i<900;i++)//allocating dat structure
 {
-int p//temporary variable to store free pid;
-for(int i=0;i<900;i++)
-{
-    if(pid_array[i]==0)
-      {
-        pid_array[i] = 1;//changing the value.
-        p = i;//allocating pid to temporary variable
-        break;
-      }
-      return p+100;//adding hundred to satisfy the constraints that pid must lie btw 100 to 1000.
-}
+pid_array[i]=0;
 }
 
-void realse_pid(int p)
+for(int i=0;i<900;i++)//checking if memory is allocated properly or not
 {
-int q = p - 100;//to get correct index of the pid
-pid_array[q] = 0//changing the value to free state.
+if(pid_array[i]!=0) result = 0;
+else result = 1;
+}
+return result;
 }
 
-void *test_threads(void *arg)//method to create threads which would request pid and then release them after a sleep
+int allocate()
+{
+int p;
+for(int i=0;i<900;i++)//checking for free pid
+{
+if(pid_array[i]==0){pid_array[i]=1;p=i;break;}
+}
+return p+100;
+}
+
+void release_pid(int p)
+{
+p = p-100;
+pid_array[p] = 0;
+}
+
+void *test_thread(void *arg)
 {
 int *s = (int *)arg;
 int process_id;
 process_id = allocate();
-printf("\tThread with Process ID : %d\n",process_id);
 sleep(*s);
-release_pid(process_id)
+printf("\tThread with Process ID: %d\n",process_id);
+release_pid(process_id);
 }
-
 
 int main()
 {
@@ -61,50 +52,137 @@ printf("\t\t\t********PROGRAM ID MANAGER********\n");
 int inp,lp=1;
 while(lp)
 {
-printf("\t1.ALLOCATE MEMORY");
-printf("\t2.CREATE THREADS");
-printf("\t3.!!!EXIT!!!")
+printf("\t1.Allocate Memory");
+printf("\t2.Create Threads");
+printf("\t3.!!!EXIT!!!");
 printf("\n");
 printf("\n");
-printf("Enter your choice:\t\n");
+printf("Enter your choice:\t");
 scanf("%d",&inp);
-
 switch(inp)
 {
 case 1:
 {
 int r = allocate_map();
-if(r==1)printf("Memory allocation successfull\n");
-else printf("Memory not allocated\n");
+if(r == 1){printf("\tMEMORY ALLOCATED SUCESSFULLY!!\n");}
+else printf("\tMEMORY NOT ALLOCATED PROPERLY\n");
 break;
 }
 
 case 2:
 {
-int sl,n;
+int sl;
 pthread_t newthread;
-printf("\tEnter the duration you want the thread to sleep:\t");
+printf("**\nEnter how much time the thread will sleep:\t");
 scanf("%d",&sl);
-printf("\nEnter the no of threads you want to create:\t");
-scanf("%d",&n);
-for(int i=0;i<n;i++)
-{
 pthread_create(&newthread,NULL,test_thread,&sl);
-}
 break;
 }
-
 case 3:
 {
 lp=0;
 break;
 }
-
 default:
 {
-printf("Wrong Input enter again#\n");
+printf("program terminated due to wrong input,select again\n");
+}
+}
+}
 }
 
+
+sneakxxage@DESKTOP-J1HT8ET:~$ ^C
+sneakxxage@DESKTOP-J1HT8ET:~$ cat  program_manager.c
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
+int pid_array[900];
+
+int allocate_map()
+{
+int result;
+for(int i=0;i<900;i++)//allocating dat structure
+{
+pid_array[i]=0;
+}
+
+for(int i=0;i<900;i++)//checking if memory is allocated properly or not
+{
+if(pid_array[i]!=0) result = 0;
+else result = 1;
+}
+return result;
+}
+
+int allocate()
+{
+int p;
+for(int i=0;i<900;i++)//checking for free pid
+{
+if(pid_array[i]==0){pid_array[i]=1;p=i;break;}
+}
+return p+100;
+}
+
+void release_pid(int p)
+{
+p = p-100;
+pid_array[p] = 0;
+}
+
+void *test_thread(void *arg)
+{
+int *s = (int *)arg;
+int process_id;
+process_id = allocate();
+sleep(*s);
+printf("\tThread with Process ID: %d\n",process_id);
+release_pid(process_id);
+}
+
+int main()
+{
+printf("\t\t\t********PROGRAM ID MANAGER********\n");
+int inp,lp=1;
+while(lp)
+{
+printf("\t1.Allocate Memory");
+printf("\t2.Create Threads");
+printf("\t3.!!!EXIT!!!");
+printf("\n");
+printf("\n");
+printf("Enter your choice:\t");
+scanf("%d",&inp);
+switch(inp)
+{
+case 1:
+{
+int r = allocate_map();
+if(r == 1){printf("\tMEMORY ALLOCATED SUCESSFULLY!!\n");}
+else printf("\tMEMORY NOT ALLOCATED PROPERLY\n");
+break;
+}
+
+case 2:
+{
+int sl;
+pthread_t newthread;
+printf("**\nEnter how much time the thread will sleep:\t");
+scanf("%d",&sl);
+pthread_create(&newthread,NULL,test_thread,&sl);
+break;
+}
+case 3:
+{
+lp=0;
+break;
+}
+default:
+{
+printf("program terminated due to wrong input,select again\n");
+}
 }
 }
 }
